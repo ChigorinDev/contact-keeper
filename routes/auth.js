007dee7559cc73cf2a76d9bae71936a8dabc03fc
MikleChigorin <chigorinmiha@gmail.com>
2019-09-14 21:05:57 +0300

    add auth API and password & email validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
+const User = require('../models/User');
 //@route  GET api/auth
 //@desc   Get Logged in user
 //@access Private
@@ -11,8 +16,56 @@ router.get('/', (req, res) => {
 //@route  POST api/auth
 //@desc   Auth user & get token
 //@access Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: 'Password or Email is incorrect' });
+    }
+
+    const { email, password } = req.body;
+
+    //If there is ont such a user - he has to register before
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+      //Compare passwords server vs user inter
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.send({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
